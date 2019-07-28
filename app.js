49063b4caafda050666e1da9e2ac1bc9fff17c2a
Victoria <45143789+Inseteco@users.noreply.github.com>
2019-07-28 20:20:39 +0100

    ComentsBox
    Creating and Deleting coments

---
+++ app.js
@@ -0,0 +1,24 @@
+new Vue ({
+  el: '#app',
+
+  data (){
+    return {
+      newComent: [],
+      coments: []   
+    }
+  },
+
+  methods: {
+
+    addText(){
+     if (this.newComent != '') {
+        this.coments.push(this.newComent);
+        this.newComent = ''
+      }
+    },
+    deleteCom(id){
+      this.coments.splice(id, 1);
+     }
+  }  
+})
+
